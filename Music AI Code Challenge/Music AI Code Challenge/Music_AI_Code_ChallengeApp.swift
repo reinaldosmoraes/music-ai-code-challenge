cd94c021961fe941bb4ad26f64ab80f00c349177
Reinaldo Moraes <reinaldosmoraes@gmail.com>
2026-05-16 11:31:28 -0300

    Xcode project creation

---
+++ Music AI Code Challenge/Music AI Code Challenge/Music_AI_Code_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AI_Code_ChallengeApp.swift
+//  Music AI Code Challenge
+//
+//  Created by Reinaldo Moraes on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Music_AI_Code_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
